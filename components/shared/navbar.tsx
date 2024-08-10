91aee8c2a6b9c49b1045a564b4d374e456d08f3c
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-08-11 00:13:11 +0500

    layout ready with mobile

---
+++ components/shared/navbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "../mobile-nav";
+
+function Navbar() {
+    return <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+        <Link className="flex items-center gap-1" href='/'>
+            <Image src='/icons/logo.svg' width={32} height={32} alt="Yoom logo" className="max-sm:size-10" />
+            <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+                Yoom
+            </p>
+        </Link>
+
+        <div className="flex-between gap-5">
+            {/* Clerk user management */}
+
+            <MobileNav/>
+        </div>
+    </nav>;
+}
+
+export default Navbar;
