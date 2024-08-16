99fcae2969a8311c35b0f85704cf502603b190a9
Sukhrob Abdullaev <s.abdullaev@tuit.uz>
2024-08-16 22:48:49 +0500

    localization

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,36 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster"
 import "@stream-io/video-react-sdk/dist/css/styles.css";
+// import { uzUZ } from '@clerk/localizations'
+
+const localization = {
+  socialButtonsBlockButton: "{{provider|titleize}} orqali davom etish",
+  userButton: {
+    action__addAccount: "Hisob qo'shish",
+    action__manageAccount: "Hisobni boshqarish",
+    action__signOut: "Chiqish",
+    action__signOutAll: "barch hisobdan chiqish",
+    actionLink: "Ro'yxatdan o'tish",
+  },
+  signIn: {
+    start: {
+      title: "Kirish",
+      subtitle: '"VIDEO CHAT"ni davom ettirish uchun',
+      actionText: "Hisobingiz yo'qmi?",
+      actionLink: "Ro'yxatdan o'tish",
+    },
+  },
+  signUp: {
+    start: {
+      title: "Hisob yarating",
+      subtitle: '"VIDEO CHAT"ni davom ettirish uchun',
+      actionText: "Hisobingiz bormi?",
+      actionLink: "Kirish",
+    },
+  },
+  formButtonPrimary: 'Email orqali kirish',
+  formFieldLabel__password: 'Parol',
+};
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,25 +45,29 @@ export const metadata: Metadata = {
   }
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
-      layout: {
-        logoImageUrl: "/icons/yoom-logo.svg",
-        socialButtonsVariant: 'iconButton'
-      },
-      variables: {
-        colorText: "white",
-        colorPrimary: "#0E78F9",
-        colorBackground: "#1C1F2e",
-        colorInputBackground: "#252a41",
-        colorInputText: "white"
-      }
-    }}>
+    <ClerkProvider
+      localization={localization}
+      appearance={{
+        layout: {
+          logoImageUrl: "/icons/yoom-logo.svg",
+          socialButtonsVariant: 'iconButton'
+        },
+        variables: {
+          colorText: "white",
+          colorPrimary: "#0E78F9",
+          colorBackground: "#364799",
+          colorInputBackground: "#252a41",
+          colorInputText: "white"
+        },
+      }}
+    >
       <html lang="en">
         <body className={`${inter.className} bg-dark-2`}>{children}
           <Toaster />
